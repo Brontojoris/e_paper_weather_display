a3cb2e9aa23899604bfcbd92618212a5f01a2c52
Joris de Beer <joris.debeer@maxisdigital.com.au>
2024-01-02 22:49:22 +1100

    Update weather.py
    Rewritten to include internet connectivity, and move/replace some of the weather data

--- weather.py
+++ weather.py
@@ -3,31 +3,44 @@
 # It uses OpenWeatherMap API to display weather info
 import sys
 import os
+import requests
 
-picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pic')
-icondir = os.path.join(picdir, 'icon')
-fontdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'font')
+from dotenv import load_dotenv
+from datetime import datetime
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+
+import json
+from io import BytesIO
+import csv
 
 # Search lib folder for display driver modules
 sys.path.append('lib')
 from waveshare_epd import epd7in3g
 epd = epd7in3g.EPD()
 
-from datetime import datetime
-import time
-from PIL import Image,ImageDraw,ImageFont
-import traceback
+picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pic')
+icondir = os.path.join(picdir, 'icon')
+fontdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'font')
 
-import requests, json
-from io import BytesIO
-import csv
-
-from dotenv import load_dotenv
 load_dotenv()
 
-# define funciton for writing image and sleeping for 5 min.
-def write_to_screen(image, sleep_seconds):
-    print('Writing to screen.')
+# define function for checking internet connection
+def internet_connection(url):
+    global internet_icon_file
+    try:
+        response = requests.get(url, timeout=5)
+        print('Checking for Internet connectivity.')
+        internet_icon_file = 'internet.png'
+        return True
+    except requests.ConnectionError:
+        internet_icon_file = 'warning.png'
+        return False
+
+# define function for writing image and sleeping for 5 min.
+def write_to_screen(image):
+    print('Writing to file.')
     # Write to screen
     h_image = Image.new('1', (epd.width, epd.height), 255)
     # Open the template
@@ -39,8 +52,6 @@ def write_to_screen(image, sleep_seconds):
     # Sleep
     time.sleep(2)
     epd.sleep()
-    print('Sleeping for ' + str(sleep_seconds) +'.')
-    time.sleep(sleep_seconds)
 
 # define function for displaying error
 def display_error(error_source):
@@ -60,7 +71,7 @@ def display_error(error_source):
     # Close error image
     error_image.close()
     # Write error to screen
-    write_to_screen(error_image_file, 30)
+    write_to_screen(error_image_file)
 
 # Set the fonts
 font22 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 22)
@@ -68,8 +79,17 @@ font30 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 30)
 font35 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 35)
 font50 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 50)
 font60 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 60)
+font70 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 70)
+font80 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 80)
+font90 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 90)
 font100 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 100)
 font160 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 160)
+font180 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 180)
+font200 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 200)
+font220 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 220)
+font240 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 240)
+font260 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 260)
+
 # Set the colors
 black = 'rgb(0,0,0)'
 white = 'rgb(255,255,255)'
@@ -92,7 +112,7 @@ URL = BASE_URL + 'lat=' + LATITUDE + '&lon=' + LONGITUDE + '&units=' + UNITS +'&
 
 while True:
     # Ensure there are no errors with connection
-    error_connect = True
+    error_connect = internet_connection('http://openweathermap.org')
     while error_connect == True:
         try:
             # HTTP request
@@ -158,13 +178,13 @@ while True:
 
             # Set strings to be printed to screen
             string_location = LOCATION
-            string_temp_current = format(temp_current, '.0f') + u'\N{DEGREE SIGN}F'
-            string_feels_like = 'Feels like: ' + format(feels_like, '.0f') +  u'\N{DEGREE SIGN}F'
+            string_temp_current = format(temp_current, '.0f') + u'\N{DEGREE SIGN}'
+            string_feels_like = format(feels_like, '.0f') +  u'\N{DEGREE SIGN}'
             string_humidity = 'Humidity: ' + str(humidity) + '%'
-            string_wind = 'Wind: ' + format(wind, '.1f') + ' MPH'
+            string_wind = 'Wind: ' + format(wind, '.1f') + ' KPH'
             string_report = 'Now: ' + report.title()
-            string_temp_max = 'High: ' + format(temp_max, '>.0f') + u'\N{DEGREE SIGN}F'
-            string_temp_min = 'Low:  ' + format(temp_min, '>.0f') + u'\N{DEGREE SIGN}F'
+            string_temp_max = format(temp_max, '>.0f') + u'\N{DEGREE SIGN}'
+            string_temp_min = format(temp_min, '>.0f') + u'\N{DEGREE SIGN}'
             string_precip_percent = 'Precip: ' + str(format(daily_precip_percent, '.0f'))  + '%'
 
             # Set error code to false
@@ -173,13 +193,13 @@ while True:
             '''
             print('Location:', LOCATION)
             print('Temperature:', format(temp_current, '.0f'), u'\N{DEGREE SIGN}F')
-            print('Feels Like:', format(feels_like, '.0f'), 'F')
+            print(format(feels_like, '.0f'), 'C')
             print('Humidity:', humidity)
-            print('Wind Speed:', format(wind_speed, '.1f'), 'MPH')
+            print('Wind Speed:', format(wind_speed, '.1f'), 'KPH')
             print('Report:', report.title())
 
-            print('High:', format(temp_max, '.0f'), 'F')
-            print('Low:', format(temp_min, '.0f'), 'F')
+            print('High:', format(temp_max, '.0f'), 'C')
+            print('Low:', format(temp_min, '.0f'), 'C')
             print('Probabilty of Precipitation: ' + str(format(daily_precip_percent, '.0f'))  + '%')
             '''
         else:
@@ -194,34 +214,30 @@ while True:
     # Draw top left box
     ## Open icon file
     icon_file = icon_code + '.png'
+    icon_size = (250,250)
     icon_image = Image.open(os.path.join(icondir, icon_file))
+    icon_image = icon_image.resize(icon_size)
     ### Paste the image
-    template.paste(icon_image, (40, 15))
+    template.paste(icon_image, (10, 15))
     ## Place a black rectangle outline
-    draw.rectangle((25, 20, 225, 180), outline=black)
+    #draw.rectangle((25, 20, 225, 180), outline=black)
     ## Draw text
-    draw.text((30, 200), string_report, font=font22, fill=black)
-    draw.text((30, 240), string_precip_percent, font=font30, fill=black)
+    #draw.text((30, 200), string_report, font=font22, fill=black)
+    #draw.text((30, 240), string_precip_percent, font=font30, fill=black)
     # Draw top right box
-    draw.text((375, 35), string_temp_current, font=font160, fill=black)
-    draw.text((350, 210), string_feels_like, font=font50, fill=black)
+    draw.text((255, 30), string_temp_current, font=font200, fill=black)
+    draw.text((62, 385), string_feels_like, font=font60, fill=black)
     # Draw bottom left box
-    draw.text((35, 325), string_temp_max, font=font50, fill=black)
-    draw.rectangle((170, 385, 265, 387), fill=black)
-    draw.text((35, 390), string_temp_min, font=font50, fill=black)
+    draw.text((664, 48), string_temp_max, font=font80, fill=black)
+    #draw.rectangle((170, 385, 265, 387), fill=black)
+    draw.text((664, 148), string_temp_min, font=font80, fill=black)
     # Draw bottom middle box
-    draw.text((345, 340), string_humidity, font=font30, fill=black)
-    draw.text((345, 400), string_wind, font=font30, fill=black)
-    # Draw bottom right box
-    draw.text((627, 330), 'UPDATED', font=font35, fill=white)
-    current_time = datetime.now().strftime('%H:%M')
-    draw.text((627, 375), current_time, font = font60, fill=white)
-
-    ## Add a reminder to take out trash on Mon and Thurs
-    weekday = datetime.today().weekday()
-    if weekday == 0 or weekday == 3:
-        draw.rectangle((345, 13, 705, 55), fill =black)
-        draw.text((355, 15), 'TAKE OUT TRASH TODAY!', font=font30, fill=white)
+    #draw.text((345, 340), string_humidity, font=font30, fill=black)
+    #draw.text((345, 400), string_wind, font=font30, fill=black)
+	
+	# Draw internet connection
+    internet_icon = Image.open(os.path.join(icondir, internet_icon_file))
+    template.paste(internet_icon, (600, 300))
 
     # Save the image for display as PNG
     screen_output_file = os.path.join(picdir, 'screen_output.png')
@@ -235,4 +251,4 @@ while True:
     	epd.Clear()
 
     # Write to screen
-    write_to_screen(screen_output_file, 600)
+    write_to_screen(screen_output_file)
